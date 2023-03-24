ce0b6554627d84a655c84df35b6227e98728943f
kzubkov <kirya.zubkov.98@list.ru>
2023-03-24 22:24:26 +0300

    feat: modify app.files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import {Home} from './pages/Home';
+import {About} from './pages/About';
+import {Store} from './pages/Store';
+import {useEffect} from 'react';
+import {Container} from '@mui/system';
+import {Navbar} from './components/Navbar';
+import {Provider} from 'react-redux';
+import {store} from './store/store';
 
 function App() {
-  return <div className='App'></div>;
+  useEffect(() => {
+    console.log('here');
+  });
+  return (
+    <Provider store={store}>
+      <div className='App'>
+        <Container sx={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/store' element={<Store />} />
+          </Routes>
+        </Container>
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
