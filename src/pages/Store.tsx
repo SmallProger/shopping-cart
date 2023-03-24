bf8bb9fe9bbbfcdad8f0229f1c7920cc34325bee
kzubkov <kirya.zubkov.98@list.ru>
2023-03-24 22:18:20 +0300

    feat: modify pages

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,19 @@
+import Paper from '@mui/material/Paper';
+import {Grid} from '@mui/material';
+import dataStoreItems from '../data/items.json';
+import MediaCard from '../components/StoreItem';
 function Store() {
-  return <div>Store</div>;
+  return (
+    <Paper sx={{padding: '40px'}}>
+      <Grid container spacing={{xs: 2, md: 3}} columns={{xs: 4, sm: 8, md: 12}}>
+        {dataStoreItems.map((itemData) => (
+          <Grid item xs={2} sm={4} md={4} key={itemData.id}>
+            {MediaCard(itemData)}
+          </Grid>
+        ))}
+      </Grid>
+    </Paper>
+  );
 }
 
 export {Store};
